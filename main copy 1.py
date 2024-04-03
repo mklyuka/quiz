556b8a90392b12b55f100291ea08d814a3642d79
mklyuka <m_klyuka@inbox.ru>
2024-04-03 13:54:54 +0300

    работает с рандомными ответами

--- main copy 1.py
+++ main copy 1.py
@@ -13,9 +13,24 @@ browser = webdriver.Chrome(options=chrome_options)
 
 # ожидание кликабельности элемента по селектору
 def el_to_be_clickable(CSS_SELECTOR):
-    en_af = WebDriverWait(browser, 100).until(
+    en_af = WebDriverWait(browser, 1200, 1).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, CSS_SELECTOR))
         )
+
+def el_to_be_clicable_XP(XPATH):
+    en_af = WebDriverWait(browser, 100, 1).until(
+            EC.element_to_be_clickable((By.XPATH, XPATH))
+        )
+
+def el_not_clicable_XP(XPATH):
+    en_af = WebDriverWait(browser, 100, 1).until(
+            EC.element_to_be_clickable((By.XPATH, XPATH))
+        )
+
+def not_presence_el_loc(CSS_SELECTOR):
+    WebDriverWait(browser, 1200).until_not(
+        EC.presence_of_element_located((By.CSS_SELECTOR, CSS_SELECTOR))
+    )
 
 #Словарь имен
 names = ['Максим', 'Михаил', 'Александр', 'Дмитрий', 'Денис', 'Илья', 'Андрей', 'Даниил', 'Артём', 'Иван', 'Алексей', 'Никита',
@@ -30,13 +45,20 @@ try:
     el_to_be_clickable('[aria-label="Ваше имя или никнейм"]')
     browser.find_element(By.CSS_SELECTOR, '[aria-label="Ваше имя или никнейм"]').send_keys(random.choice(names))
 
-    time.sleep(3)
-
     # Клик по кнопке ДАЛЕЕ
     el_to_be_clickable('.q-btn__content')
     browser.find_element(By.CSS_SELECTOR, '.q-btn__content').click()
 
-    time.sleep(3)
+    for i in range(50):
+        el_to_be_clickable('[style=""]')
+        time.sleep(random.randint(4, 12))
+
+        browser.find_element(By.CSS_SELECTOR, '.flex.fit>.col>button:nth-child({})'.format(random.randint(1, 4))).click()
+
+        el_to_be_clicable_XP("//div[text()=' Ожидайте следующий вопрос ']")
+        el_not_clicable_XP("//div[text()=' Ожидайте следующий вопрос ']")
+
+
 
 finally:
     time.sleep(5)
